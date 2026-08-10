225ec3a3d6e7d933b78ace75b78fcc561e9688a3
DJY <djyxstudio@163.com>
2026-08-10 14:17:16 +0800

    release: 1.1.0

--- backend/common.py
+++ backend/common.py
@@ -50,7 +50,7 @@ from winotify import Notification, audio
 
 APP_NAME = "DJYX_APITOOL"
 WINDOW_TITLE = "DJYX_APITOOL"
-APP_VERSION = "1.0.30"
+APP_VERSION = "1.1.0"
 TITLE_BAR_MODES = {"default", "minimal", "original"}
 BACKGROUND_UI_MODES = {"delayed", "active", "low_power"}
 GITHUB_REPOSITORY = os.environ.get(
